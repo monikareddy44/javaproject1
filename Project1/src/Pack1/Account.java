80528fa0afaf060a08491b7b5a3d47ba4cffc390
acer <acer@sonika>
2020-11-03 10:40:26 +0530

    first commit

---
+++ Project1/src/Pack1/Account.java
@@ -0,0 +1,32 @@
+package Pack1;
+
+public class Account {
+	long accNum;
+	double balance;
+	private static long long accNum2;
+	
+	public long getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(long accNum) {
+		this.accNum = accNum;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+public static void main(String[] args) {
+	account=;
+	
+	
+	
+}
+	
+}	
+	 
+		
